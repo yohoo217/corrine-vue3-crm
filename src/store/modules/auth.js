5002584d3bfa186abf341ada48173a84b6e2aa6f
JyunYan Chen <ian.chen@aotter.net>
2024-10-13 17:59:01 +0800

    解決了登出後跳轉頁面錯誤問題

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,19 +1,21 @@
 // src/store/modules/auth.js
+import router from '@/router';
+
 const state = {
   token: localStorage.getItem('token') || '',
-  role: localStorage.getItem('role') || 'guest', // 默认状态为 guest
+  role: localStorage.getItem('role') || 'guest',
 };
 
 const mutations = {
   SET_TOKEN(state, { token, role }) {
     state.token = token;
     state.role = role;
-    localStorage.setItem('token', token); // 存储 token
-    localStorage.setItem('role', role);   // 存储角色
+    localStorage.setItem('token', token);
+    localStorage.setItem('role', role);
   },
   CLEAR_TOKEN(state) {
     state.token = '';
-    state.role = 'guest'; // 清除后状态重置为 guest
+    state.role = 'guest';
     localStorage.removeItem('token');
     localStorage.removeItem('role');
   },
@@ -21,22 +23,18 @@ const mutations = {
 
 const actions = {
   login({ commit }, { token, role }) {
-    commit('SET_TOKEN', { token, role }); // 更新状态为登录
+    commit('SET_TOKEN', { token, role });
   },
   async logout({ commit }) {
-    // 清除用戶數據和 token
-    commit('setUser', null);
-    commit('setToken', null);
-    localStorage.removeItem('token');
-    
-    // 這裡不進行路由導航,我們將在組件中處理
-    return true; // 表示登出成功
+    commit('CLEAR_TOKEN');
+    await router.push('/'); // 使用 await 確保路由變更完成
+    window.location.reload(); // 重新加載頁面
   },
 };
 
 const getters = {
-  isAuthenticated: state => !!state.token, // 返回 true 表示已登录
-  userRole: state => state.role, // 获取用户角色
+  isAuthenticated: state => !!state.token,
+  userRole: state => state.role,
 };
 
 export default {
