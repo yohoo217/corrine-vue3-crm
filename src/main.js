1c7803d90fcaf58f676b91dbfa97599b3b47b960
JyunYan Chen <ian.chen@aotter.net>
2024-10-31 22:20:51 +0800

    加上語言切換功能

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
 import axios from 'axios'
+import i18n from './i18n' // 引入 i18n
+
 
 // PrimeVue styles
 import 'primevue/resources/themes/saga-blue/theme.css'
@@ -14,33 +16,23 @@ import 'primevue/resources/primevue.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
-
 // 設置 Axios 的基礎 URL
 axios.defaults.baseURL = 'http://localhost:5001/api'
 
 const app = createApp(App)
 
+// 配置 Vue 的全局錯誤處理器
 app.config.errorHandler = (err, vm, info) => {
-    console.error('Vue error:', err)
-    console.log('Error info:', info)
-  }
-  
+  console.error('Vue error:', err)
+  console.log('Error info:', info)
+}
 
+// 使用 Vue 插件和服務
 app.use(router)
 app.use(store)
 app.use(PrimeVue)
 app.use(ToastService)
 app.use(ConfirmationService)
+app.use(i18n) // 使用 i18n
 
 app.mount('#app')
-
-  
-  app.use(store)
-     .use(router)
-     .use(PrimeVue)
-     .use(PrimeVue, { ripple: true })
-    
-  app.use(PrimeVue, {
-      ripple: true,
-      inputStyle: 'outlined'
-    })
