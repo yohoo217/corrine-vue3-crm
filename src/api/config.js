0df955957c299b1e961b5197bd63968f349834ba
JyunYan Chen <ian.chen@aotter.net>
2024-11-23 19:01:26 +0800

    fix call api /me 無法拿到使用者資料

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://wangcom.online/api',
+  baseURL: process.env.NODE_ENV === "production" 
+    ? "https://wangcom.online/api" // 正式環境的 API 路徑
+    : "http://localhost:5001/api", // 本地開發的 API 路徑
   headers: {
     'Content-Type': 'application/json'
   },
